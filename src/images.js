c3b5a6bdf70994c73c608967fa74cad7a34de30f
Catherine Swihart <catherineswihart@gmail.com>
2019-12-19 19:15:32 -0600

    app complete

---
+++ src/images.js
@@ -0,0 +1,27 @@
+import family from './images/family.jpg';
+import bob from './images/bob.jpg';
+import gayle from './images/gayle.jpg';
+import gene from './images/gene.jpg';
+import jimmy from './images/jimmy.jpg';
+import linda from './images/linda.jpg';
+import louise from './images/louise.jpg';
+import tammy from './images/tammy.png';
+import teddy from './images/teddy.jpg';
+import tina from './images/tina.jpg';
+import zeke from './images/zeke.jpg';
+
+const images = [
+   family,
+   bob,
+   gayle,
+   gene,
+   jimmy,
+   linda,
+   louise,
+   tammy,
+   teddy,
+   tina,
+   zeke
+];
+
+export default images;
